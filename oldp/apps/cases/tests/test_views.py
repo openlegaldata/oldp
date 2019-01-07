d628b5408d6f2990950c6c8e34b1585d2dc8ebb3
mschwarzer <gitlab@i.mieo.de>
2019-01-07 14:22:10 +0100

    Added simple view tests

--- oldp/apps/cases/tests/test_views.py
+++ oldp/apps/cases/tests/test_views.py
@@ -1,13 +1,34 @@
 from django.test import LiveServerTestCase, tag
 from django.urls import reverse
 
+from oldp.apps.cases.models import Case
+
 
 @tag('views')
 class CasesViewsTestCase(LiveServerTestCase):
-    fixtures = ['locations/countries.json', 'locations/states.json', 'locations/cities.json', 'courts/courts.json']
+    fixtures = [
+        'locations/countries.json',
+        'locations/states.json',
+        'locations/cities.json',
+        'courts/courts.json',
+        'cases/cases.json'
+    ]
 
     def test_index(self):
         res = self.client.get(reverse('cases:index'))
 
         self.assertEqual(res.status_code, 200)
 
+    def test_detail(self):
+        item = Case.objects.get(pk=1)
+
+        res = self.client.get(item.get_absolute_url())
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_short_url(self):
+        item = Case.objects.get(pk=1)
+
+        res = self.client.get(item.get_short_url())
+
+        self.assertRedirects(res, item.get_absolute_url(), status_code=301)
