524ee53467d40b58600b6ec01f406d6b752742bf
mschwarzer <gitlab@i.mieo.de>
2019-01-31 13:06:36 +0100

    More tests

--- oldp/apps/cases/tests/test_views.py
+++ oldp/apps/cases/tests/test_views.py
@@ -1,4 +1,7 @@
-from django.test import tag
+from unittest import skip
+
+from django.contrib.auth.models import User
+from django.test import tag, Client
 from django.urls import reverse
 
 from oldp.apps.cases.models import Case
@@ -14,6 +17,20 @@ class CasesViewsTestCase(ExtendedLiveServerTestCase):
         'courts/courts.json',
         'cases/cases.json'
     ]
+    username = 'test'
+    password = 'test'
+
+    def setUp(self):
+        self.user = User.objects.create_user(self.username, 'test@example.com', self.password)
+
+        self.user_client = Client()
+        self.user_client.force_login(self.user)
+
+        self.staff_user = User.objects.create_user('staff', 'staff@example.com', 'staff', is_staff=True)
+
+        self.staff_client = Client()
+        self.staff_client.force_login(self.staff_user)
+
 
     def test_index(self):
         res = self.client.get(reverse('cases:index'))
@@ -37,6 +54,22 @@ class CasesViewsTestCase(ExtendedLiveServerTestCase):
         res = self.client.get(item.get_absolute_url())
 
         self.assertContains(res, item.get_content_as_html(), count=1, status_code=200)
+
+    @skip # requires correct status codes
+    def test_private_detail(self):
+        private_item = Case.objects.get(pk=2)
+        res = self.client.get(private_item.get_absolute_url())
+
+        self.assertEqual(404, res.status_code, 'Private content should not be found with anon client')
+
+        res = self.staff_client.get(private_item.get_absolute_url())
+
+        self.assertEqual(200, res.status_code, 'Private content must be visible to staff users')
+
+    # def test_detail_as_user(self):
+    #     # TODO
+    #     pass
+    #     self.client.request()
 
     def test_short_url(self):
         item = Case.objects.get(pk=1)
