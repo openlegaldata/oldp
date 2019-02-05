6bd954830252b93af7b4ac4cef9fd9dc89afe308
langhabel <langhabel@users.noreply.github.com>
2019-02-05 09:50:59 +0100

    Replace language model shortcut link with actual name

--- oldp/apps/nlp/tests/ner/test_entity_relations.py
+++ oldp/apps/nlp/tests/ner/test_entity_relations.py
@@ -11,7 +11,7 @@ class EntityRelationsTestCase(TestCase):
     @skip
     def test_extract_pobj_en(self):
         text = 'The man resides in the USA.'
-        nlp = spacy.load('en')
+        nlp = spacy.load('en_core_web_sm')
         doc = nlp(text)
         expected_relations = [(('man', 'reside'), ('USA', 23, 25))]
         relations = extract_relations(doc, 'GPE', 'en_core_web_sm')
@@ -20,7 +20,7 @@ class EntityRelationsTestCase(TestCase):
     @skip
     def test_extract_dobj_en(self):
         text = 'The cat owns 1000 €.'
-        nlp = spacy.load('en')
+        nlp = spacy.load('en_core_web_sm')
         doc = nlp(text)
         expected_relations = [(('cat', 'own'), ('1000 €', 13, 18))]
         relations = extract_relations(doc, 'MONEY', 'en_core_web_sm')
@@ -28,7 +28,7 @@ class EntityRelationsTestCase(TestCase):
 
     def test_extract_relations_oa_de(self):
         text = 'Der Hund begrüßt Anton.'
-        nlp = spacy.load('de')
+        nlp = spacy.load('de_core_news_sm')
         doc = nlp(text)
         expected_relations = [(('Hund', 'begrüßen'), ('Anton', 17, 21))]
         relations = extract_relations(doc, 'PER', 'de_core_news_sm')
@@ -36,7 +36,7 @@ class EntityRelationsTestCase(TestCase):
 
     def test_extract_relations_nk_de(self):
         text = 'Der Vogel fliegt verbringt den Winter in Afrika.'
-        nlp = spacy.load('de')
+        nlp = spacy.load('de_core_news_sm')
         doc = nlp(text)
         expected_relations = [(('Vogel', 'in'), ('Afrika', 41, 46))]
         relations = extract_relations(doc, 'LOC', 'de_core_news_sm')
