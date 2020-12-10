53e0b750f9a74fda05b2235b2cbc6cbbad7f1ec6
malteos <7977470+malteos@users.noreply.github.com>
2020-12-10 11:17:47 +0100

    Updated dump script

--- sbin/dumpdata.sh
+++ sbin/dumpdata.sh
@@ -3,23 +3,43 @@
 # For large databases a direct mysql-dump is recommended
 # mysqldump -u ${DATABASE_LOGIN} -p${DATABASE_PASSWORD} -h 127.0.0.1 --verbose ${DATABASE_NAME} | gzip > db.sql.gz
 
-export DIR=workingdir/dumpdata
+TYPE=$1
+TODAY=$(date +'%Y-%m-%d')
+DUMP_DIR=./workingdir/dumpdata/$TODAY
 
-mkdir $DIR
+echo "Generating dump at $DUMP_DIR ..."
 
-python manage.py dumpdata sites -o $DIR/sites.json
-python manage.py dumpdata flatpages -o $DIR/flatpages.json
-python manage.py dumpdata auth -o $DIR/auth.json
-python manage.py dumpdata authtoken -o $DIR/authtoken.json
+mkdir -p $DUMP_DIR
 
-python manage.py dumpdata accounts -o $DIR/accounts.json
-python manage.py dumpdata courts -o $DIR/courts.json
-python manage.py dumpdata cases -o $DIR/cases.json
-python manage.py dumpdata laws -o $DIR/laws.json
-python manage.py dumpdata references -o $DIR/references.json
+echo "Dumping public data"
 
+python manage.py dumpdata courts -o $DUMP_DIR/courts.json
+python manage.py dumpdata cases -o $DUMP_DIR/cases.json
+python manage.py dumpdata laws -o $DUMP_DIR/laws.json
+python manage.py dumpdata references -o $DUMP_DIR/references.json
 
-zip -r workingdir/dumpdata.zip $DIR/sites.json $DIR/flatpages.json $DIR/auth.json $DIR/authtoken.json $DIR/accounts.json \
-    $DIR/courts.json $DIR/cases.json $DIR/laws.json $DIR/references.json
+if [[ $TYPE == "full" ]]
+then
+    echo "Including production data in dump..."
 
-rm -r $DIR
+    python manage.py dumpdata sites -o $DUMP_DIR/sites.json
+    python manage.py dumpdata flatpages -o $DUMP_DIR/flatpages.json
+    python manage.py dumpdata auth -o $DUMP_DIR/auth.json
+    python manage.py dumpdata authtoken -o $DUMP_DIR/authtoken.json
+    python manage.py dumpdata accounts -o $DUMP_DIR/accounts.json
+
+    OUT_FILE="oldp_dump.full.$TODAY.zip"
+
+else
+    OUT_FILE="oldp_dump.public.$TODAY.zip"
+fi
+
+echo "Zipping all the files.."
+
+zip -r ./workingdir/$OUT_FILE $DUMP_DIR/*
+
+echo "Cleaning dump dir..."
+
+rm -r $DUMP_DIR
+
+echo "Done."
